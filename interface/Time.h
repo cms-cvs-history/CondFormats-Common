06f949b7752df8b96427d938bb3ea605658c418f
cmsbuild <>
2009-02-27 15:55:19 +0000

    This commit was manufactured by cvs2git to create tag 'CondFormats- Common-V08-00-01'.
    Sprout from master 2009-02-19 11:29:26 UTC Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch> 'added'
    Cherrypick from master 2009-02-27 15:55:18 UTC Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch> 'outline':
        interface/Time.h
        src/Time.cc

--- interface/Time.h
+++ interface/Time.h
@@ -5,37 +5,35 @@
 #include <limits>
 #include <boost/cstdint.hpp>
 
-// to be fixed
-#include "CondCore/DBCommon/interface/Exception.h"
 
 
 namespace cond{
-
-
+  
+  
   typedef uint64_t  Time_t;
   typedef std::pair<unsigned int, unsigned int> UnpackedTime;
-
+  
   typedef std::pair<Time_t, Time_t> ValidityInterval;
-
+  
   typedef enum { runnumber=0, timestamp, lumiid, userid } TimeType;
   const unsigned int TIMETYPE_LIST_MAX=4;
-
+  
   const cond::TimeType timeTypeList[TIMETYPE_LIST_MAX]=
     {runnumber,timestamp,lumiid,userid};
-
+  
   const cond::TimeType timeTypeValues[]=
     {runnumber,timestamp,lumiid,userid};
-
+  
   const std::string timeTypeNames[]=
     {"runnumber","timestamp","lumiid","userid"};
-
-
+  
+  
   const Time_t TIMELIMIT(std::numeric_limits<Time_t>::max());
-
+  
   template<TimeType type>
   struct RealTimeType {
   };
-
+  
   
   struct TimeTypeSpecs {
     // the enum
@@ -48,28 +46,28 @@ namespace cond{
     Time_t invalidValue;
     
   }; 
-
+  
   
   template<> struct RealTimeType<runnumber> {
     // 0, run number
     typedef unsigned int type; 
   };
-
+  
   template<> struct RealTimeType<timestamp> {
     // sec, nanosec
     typedef uint64_t  type; 
   };
   
- 
+  
   template<> struct RealTimeType<lumiid> {
     // run, lumi-seg
     typedef uint64_t  type; 
   };
-
+  
   template<> struct RealTimeType<userid> {
     typedef uint64_t  type; 
   };
-
+  
   
   template<TimeType type>
   struct TimeTypeTraits {
@@ -84,23 +82,13 @@ namespace cond{
       return local;
     }
   };
-
-  const TimeTypeSpecs timeTypeSpecs[] = {
-    TimeTypeTraits<runnumber>::specs(),
-    TimeTypeTraits<timestamp>::specs(),
-    TimeTypeTraits<lumiid>::specs(),
-    TimeTypeTraits<userid>::specs(),
-  };
-
+  
+  extern const TimeTypeSpecs timeTypeSpecs[];
+  
   // find spec by name
-  inline const TimeTypeSpecs & findSpecs(std::string const & name) {
-    size_t i=0;
-    for (; i<TIMETYPE_LIST_MAX; i++)
-      if (name==timeTypeSpecs[i].name) return timeTypeSpecs[i];
-    throw cond::Exception("invalid timetype: "+name);
-    return timeTypeSpecs[0]; // compiler happy
-  }
+  const TimeTypeSpecs & findSpecs(std::string const & name);
+  
 
 }
-
 #endif
+  
---
+++ src/Time.cc
@@ -0,0 +1,28 @@
+#include "CondFormats/Common/interface/Time.h"
+
+
+
+#include "FWCore/Utilities/interface/Exception.h"
+
+namespace cond{
+
+
+ const TimeTypeSpecs timeTypeSpecs[] = {
+    TimeTypeTraits<runnumber>::specs(),
+    TimeTypeTraits<timestamp>::specs(),
+    TimeTypeTraits<lumiid>::specs(),
+    TimeTypeTraits<userid>::specs(),
+  };
+
+
+  
+  // find spec by name
+  const TimeTypeSpecs & findSpecs(std::string const & name) {
+    size_t i=0;
+    for (; i<TIMETYPE_LIST_MAX; i++)
+      if (name==timeTypeSpecs[i].name) return timeTypeSpecs[i];
+    throw cms::Exception("invalid timetype: "+name);
+    return timeTypeSpecs[0]; // compiler happy
+  }
+
+}
